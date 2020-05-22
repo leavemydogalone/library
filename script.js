c2b77a1a3fd9a0a9236692c34a4de941bd2a1743
Leavemydogalone <niklasport88@gmail.com>
2020-05-22 14:17:02 -0700

    not quite there

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let myLibrary = [];
 let container = document.querySelector('.container');
 let bookBox = document.querySelector('.book');
-// let template = function(author, )
+let addBookButton = document.querySelector('.addBookButton');
+addBookButton.addEventListener ('click', addBookToLibrary);
 
-
-
-
-
+let book1 = new Book ('phil fartman', 'the fartening', 300, 'yes');
+let book2 = new Book ('guy person', 'happy cats', 256, 'no');
+myLibrary.push(book1, book2);
 
 function Book(author, title, pages, read) {
   this.author = author,
@@ -16,27 +16,45 @@ function Book(author, title, pages, read) {
 };
 
 function addBookToLibrary() {
-  let newBook = new Book();
+  let title = prompt ('What is the title of the book?','');
+  let author = prompt ('Who is the author?', '');
+  let pages = prompt('How many pages is the book?', '');
+  let read = prompt('Have you read this book? Yes or No.', '');
+  let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-}
-
-let book1 = new Book ('phil fartman', 'the fartening', 300, 'yes');
-let book2 = new Book ('guy person', 'happy cats', 256, 'no');
-myLibrary.push(book1, book2);
-console.table(myLibrary);
+  container.textContent = '';
+  render();
+};
 
 let render = function () {
     for (i = 0; i < myLibrary.length; i++) {
         let newBox = document.createElement('newBox');
         newBox.classList.add('book');
+        newBox.setAttribute('id', `book${i}`);
         for (let key in myLibrary[i]) {
             let newPara = document.createElement('p');
             newPara.textContent = `${key}: ${myLibrary[i][key]}`;
             newBox.appendChild(newPara);
-            
         };
+        let removeButton = document.createElement('button');
+        removeButton.setAttribute('class', 'removeButton')
+        removeButton.textContent = 'Remove';
+        newBox.appendChild(removeButton);
         container.appendChild(newBox);
     }
 }
+
+// let buttonRemove = document.querySelector('.removeButton');
+// buttonRemove.addEventListener('click', (e) => {
+//         let parent = e.target.parentElement;
+//         let boxID = parent.getAttribute('id').toString().charAt(4);
+//         myLibrary = myLibrary.splice(boxID, 1);
+//         alert(boxID);
+//         container.textContent = '';
+//         render();
+    
+// });
+
+
 
 render();
